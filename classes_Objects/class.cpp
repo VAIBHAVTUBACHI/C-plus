3cba4eb0f77a8e7d18d41f0ef241b978ea5444db
VAIBHAVTUBACHI <vtubachi@gmail.com>
2024-09-11 22:52:13 +0530

    Classes

---
+++ classes_Objects/class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+/*
+What is objects?
+->Objects are entities in the real world
+ 
+What is a class?
+-> Class is a blueprint of these entities
+*/
+class car{
+  public:     // datas and methods are accessible to everyone
+      int a;
+  private:    // datas and methods are accessible inside class
+      int car_VIN;
+};
+
+int main()
+{
+
+   return 0;
+}
