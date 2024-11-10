b75412ceb751c4ecdc05a4e58085fc6238f2de3b
VAIBHAVTUBACHI <vtubachi@gmail.com>
2024-11-10 14:30:10 +0530

    Inheritance_Multiple

---
+++ classes_Objects/Inheritance_mutiple.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+class Teacher{
+      public:
+      int roll_no;
+};
+class student{
+    public:
+  string name;
+};
+class gradstudent : public Teacher,public student // mutiple classes are inherited
+{
+    public:
+    string subject;
+};
+int main()
+{
+    gradstudent g1;
+     g1.name="vaibhav";
+     g1.roll_no=90;
+     g1.subject="maths";
+     cout<<g1.name<<g1.roll_no<<g1.subject;
+
+}
